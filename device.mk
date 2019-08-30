62439f7f1492d66baa7037fa35bd6c36d63a2b08
saurabhchardereal <saurabhchardereal@gmail.com>
2019-08-30 14:37:32 +0000

    beast bringup

--- device.mk
+++ device.mk
@@ -334,8 +334,12 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
+# OTA
+PRODUCT_PACKAGES += \
+    Updates
+
+#PRODUCT_ENFORCE_RRO_TARGETS := \
+   # framework-res
 
 # Power
 PRODUCT_PACKAGES += \
